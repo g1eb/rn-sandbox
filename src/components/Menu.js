ea7025745c1bb47861b0956911a87067c0f3843f
Gleb <git@gleb.nl>
2016-08-28 00:14:43 +0200

    Added initial menu component.

---
+++ src/components/Menu.js
@@ -0,0 +1,61 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+import Button from './Button';
+
+class Menu extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Menu
+        </Text>
+        <View style={styles.button}>
+          <Button backgroundColor="gold" activeColor="rgba(255, 255, 255, 0.25)" text="state 1" />
+        </View>
+        <View style={styles.button}>
+          <Button backgroundColor="gold" activeColor="rgba(255, 255, 255, 0.25)" text="state 2" />
+        </View>
+        <View style={styles.button}>
+          <Button backgroundColor="gold" activeColor="rgba(255, 255, 255, 0.25)" text="state 3" />
+        </View>
+        <View style={styles.button}>
+          <Button backgroundColor="gold" activeColor="rgba(255, 255, 255, 0.25)" text="state 4" />
+        </View>
+        <View style={styles.button}>
+          <Button backgroundColor="gold" activeColor="rgba(255, 255, 255, 0.25)" text="state 5" />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'coral',
+  },
+  title: {
+    height: 150,
+    fontSize: 52,
+    color: '#333',
+    fontFamily: 'Helvetica',
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 50,
+  }
+})
+
+module.exports = Menu
