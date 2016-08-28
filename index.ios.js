d2365e0d6ed238349e87264ea9353abfc574426d
Gleb <git@gleb.nl>
2016-08-28 05:11:31 +0200

    Cleaned up main index file to return router utility.

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict'
 
 import React, { Component } from 'react'
-import { AppRegistry, Navigator } from 'react-native'
-
-import Menu from './src/views/Menu'
-import Detail from './src/views/Detail'
-import Error from './src/views/Error'
-
-const menuItems = [
-  { id: '1', name: 'State 1', desc: 'add description here..', stateTransition: Navigator.SceneConfigs.FloatFromRight, backgroundColor: 'lightgoldenrodyellow' },
-  { id: '2', name: 'State 2', desc: 'add description here..', stateTransition: Navigator.SceneConfigs.FloatFromLeft, backgroundColor: 'mistyrose' },
-  { id: '3', name: 'State 3', desc: 'add description here..', stateTransition: Navigator.SceneConfigs.FloatFromBottom, backgroundColor: 'peachpuff' },
-  { id: '4', name: 'State 4', desc: 'add description here..', stateTransition: Navigator.SceneConfigs.VerticalUpSwipeJump, backgroundColor: 'antiquewhite' },
-  { id: '5', name: 'State 5', desc: 'add description here..', stateTransition: Navigator.SceneConfigs.VerticalDownSwipeJump, backgroundColor: 'bisque' },
-]
+import { AppRegistry } from 'react-native'
+import Router from './src/utils/Router'
 
 class sandbox extends Component {
-
-  renderScene(route, navigator) {
-    var navigatorProps = { navigator }
-    switch (route.id) {
-      case 'menu':
-        return <Menu items={menuItems} {...navigatorProps} />
-        break
-      case 'detail':
-        return <Detail item={route.item} {...navigatorProps} />
-        break
-      default:
-        return <Error {...navigatorProps} />
-        break
-    }
-  }
-
   render() {
-    return (
-      <Navigator
-        initialRoute={{ id: 'menu' }}
-        renderScene={this.renderScene}
-        configureScene={(route) => ({
-          ...route.sceneConfigs || Navigator.SceneConfigs.FloatFromRight })} />
-    )
+    return <Router />
   }
 }
 
