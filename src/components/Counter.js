d5d83f3169b548231e09e682e062f82a1d373338
Gleb <git@gleb.nl>
2016-08-28 15:52:27 +0200

    Added initial counter component.

---
+++ src/components/Counter.js
@@ -0,0 +1,78 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native'
+
+import Button from 'sandbox/src/components/Button'
+
+class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+
+  _incrementCounter () {
+    this.setState({
+      counter: this.state.counter+=1
+    });
+  }
+
+  _decrementCounter () {
+    this.setState({
+      counter: this.state.counter-=1
+    });
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.counter}>{this.state.counter}</Text>
+        <View style={styles.actions}>
+          <Button
+            text="More"
+            icon="arrow-upward"
+            iconAlign="left"
+            backgroundColor="whitesmoke"
+            activeColor="rgba(245, 245, 245, 0.5)"
+            onPress={() => this._incrementCounter()} />
+          <Button
+            text="Less"
+            icon="arrow-downward"
+            iconAlign="right"
+            backgroundColor="whitesmoke"
+            activeColor="rgba(245, 245, 245, 0.5)"
+            onPress={() => this._decrementCounter()} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counter: {
+    height: 150,
+    fontSize: 100,
+    fontFamily: 'Helvetica',
+  },
+  actions: {
+    height: 50,
+    marginBottom: 100,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  }
+})
+
+module.exports = Counter
