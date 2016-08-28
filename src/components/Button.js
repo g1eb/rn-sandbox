77c744cf2047a1fcf7b8d4b12bb6fbe203b522a7
Gleb <git@gleb.nl>
2016-08-28 14:28:55 +0200

    Added icon to the button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,10 @@ import {
   StyleSheet,
   TouchableHighlight,
   Text,
+  View,
 } from 'react-native'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import _ from 'lodash'
 
@@ -33,11 +36,13 @@ class Button extends Component {
         onHideUnderlay={this._onHideUnderlay.bind(this)}
         onShowUnderlay={this._onShowUnderlay.bind(this)}
         ref={component => this._root = component} {...this.props}>
-
-        <Text style={[styles.buttonText, {color: this.props.color} ]}>
-          {_.capitalize(this.props.text)}
-        </Text>
-
+        <View style={styles.buttonWrapper}>
+          <Icon style={[styles.buttonIcon, {color: this.props.color}]} name={this.props.icon} size={25} />
+          <View style={{flex: 1}} />
+          <Text style={[styles.buttonText, {color: this.props.color}]}>
+            {_.capitalize(this.props.text)}
+          </Text>
+        </View>
       </TouchableHighlight>
     )
   }
@@ -58,6 +63,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     opacity: 0.85,
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  buttonIcon: {
+    color: '#333',
   },
   buttonText: {
     fontSize: 24,
