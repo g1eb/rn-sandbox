58f7451322043e4ef01832be301732e207ad8594
Gleb <git@gleb.nl>
2016-08-30 11:29:00 +0200

    Added initial navigateception state.

---
+++ src/views/Navigateception.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+
+import Container from 'sandbox/src/components/Container'
+import StatusBar from 'sandbox/src/components/StatusBar'
+import Header from 'sandbox/src/components/Header'
+
+class Navigateception extends Component {
+  render() {
+    return (
+      <Container style={styles.container}>
+        <StatusBar style={{backgroundColor: 'whitesmoke'}} />
+        <Header title={this.props.item.name} subtitle={this.props.item.desc} />
+      </Container>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'skyblue',
+  }
+})
+
+module.exports = Navigateception
